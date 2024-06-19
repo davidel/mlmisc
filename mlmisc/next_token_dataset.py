ea522a08e8c32335cc60f3d5d989c3e959becbc6
Davide Libenzi <davide.libenzi@gmail.com>
2024-06-19 13:55:58 +0200

    Allow custom seqence lengths.

--- mlmisc/next_token_dataset.py
+++ mlmisc/next_token_dataset.py
@@ -3,15 +3,16 @@ import torch
 
 class NextTokenDataset(torch.utils.data.Dataset):
 
-  def __init__(self, data, block_size):
+  def __init__(self, data, block_size, seqlen=1):
     self.data = data
     self.block_size = block_size
+    self.seqlen = seqlen
 
   def __len__(self):
-    return len(self.data) - self.block_size
+    return max(len(self.data) - self.block_size - self.seqlen, 0)
 
   def __getitem__(self, i):
     offset = i + self.block_size
 
-    return self.data[i: offset], self.data[offset: offset + 1]
+    return self.data[i: offset], self.data[offset: offset + self.seqlen]
 
