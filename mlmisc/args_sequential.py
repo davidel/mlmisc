62facc0ebf0dace408fa8386cd08ea5bac00bb2e
Davide Libenzi <davide.libenzi@gmail.com>
2024-06-19 17:59:39 +0200

    Simplified code.

--- mlmisc/args_sequential.py
+++ mlmisc/args_sequential.py
@@ -4,9 +4,6 @@ import torch.nn as nn
 
 class ArgsSequential(nn.Sequential):
 
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-
   def forward(self, x, *args, **kwargs):
     for mod in self:
       x = mod(x, *args, **kwargs)
