0031690618bf80518b9a19fd91b607e1d78c05e4
Davide Libenzi <davide.libenzi@gmail.com>
2024-07-03 11:08:35 +0200

    Allow padding.

--- mlmisc/next_token_dataset.py
+++ mlmisc/next_token_dataset.py
@@ -1,18 +1,24 @@
 import torch
+import torch.nn.functional as F
 
 
 class NextTokenDataset(torch.utils.data.Dataset):
 
-  def __init__(self, data, block_size, seqlen=1):
+  def __init__(self, data, block_size, seqlen=1, pad=None):
     self.data = data
     self.block_size = block_size
     self.seqlen = seqlen
+    self.pad = pad
 
   def __len__(self):
     return max(len(self.data) - self.block_size - self.seqlen, 0)
 
   def __getitem__(self, i):
     offset = i + self.block_size
+    x, y = self.data[i: offset], self.data[offset: offset + self.seqlen]
 
-    return self.data[i: offset], self.data[offset: offset + self.seqlen]
+    if self.pad is not None:
+      x = F.pad(x, self.pad['pad'], self.pad['value'])
+
+    return x, y
 
