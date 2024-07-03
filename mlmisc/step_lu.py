5eeae6a726e0323b65c63532cc93123778edafde
Davide Libenzi <davide.libenzi@gmail.com>
2024-07-03 16:43:30 +0200

    Enable left/right threshold.

--- mlmisc/step_lu.py
+++ mlmisc/step_lu.py
@@ -4,14 +4,18 @@ import torch.nn as nn
 
 class StepLU(nn.Module):
 
-  def __init__(self, threshold=None):
+  def __init__(self, threshold=None, lthreshold=None, rthreshold=None):
     super().__init__()
-    self.threshold = threshold or 0.5
+    if threshold is not None:
+      self.lthreshold, self.rthreshold = threshold, threshold
+    else:
+      self.lthreshold = lthreshold or 0.25
+      self.rthreshold = rthreshold or 0.25
 
   def forward(self, x):
     zero = torch.tensor(0.0, dtype=x.dtype, device=x.device)
-    pv = torch.maximum(x - self.threshold, zero)
-    nv = torch.minimum(x + self.threshold, zero)
+    pv = torch.maximum(x - self.rthreshold, zero)
+    nv = torch.minimum(x + self.lthreshold, zero)
 
     return torch.where(x >= zero, pv, nv)
 
