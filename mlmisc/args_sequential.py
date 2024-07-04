1c0184d3e05c274a854addb5baf6b6b32e8962af
Davide Libenzi <davide.libenzi@gmail.com>
2024-07-04 07:05:12 +0200

    Minor feature addition.

--- mlmisc/args_sequential.py
+++ mlmisc/args_sequential.py
@@ -4,6 +4,15 @@ import torch.nn as nn
 
 class ArgsSequential(nn.Sequential):
 
+  def __init__(self, *args, **kwargs):
+    if not args:
+      # Since from Python 3.7 keyword arguments are ordered, allow building a
+      # Sequential directly from the passed keyword arguments without explicitly
+      # instantiating an OrderedDict.
+      super().__init__(kwargs)
+    else:
+      super().__init__(*args, **kwargs)
+
   def forward(self, x, *args, **kwargs):
     for mod in self:
       x = mod(x, *args, **kwargs)
