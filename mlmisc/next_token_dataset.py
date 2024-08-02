c657983fe1e3c595a320d5f9ed6a87d25e2ab72b
Davide Libenzi <davide.libenzi@gmail.com>
2024-08-02 15:14:42 +0200

    Minor refactoring.

--- mlmisc/next_token_dataset.py
+++ mlmisc/next_token_dataset.py
@@ -4,20 +4,20 @@ import torch.nn.functional as F
 
 class NextTokenDataset(torch.utils.data.Dataset):
 
-  def __init__(self, data, block_size, seqlen=1, pad=None):
+  def __init__(self, data, context_size, seqlen=1, pad=None):
     pad_size = sum(pad['pad']) if pad is not None else 0
 
     super().__init__()
     self.data = data
-    self.block_size = block_size - pad_size
+    self.context_size = context_size - pad_size
     self.seqlen = seqlen
     self.pad = pad
 
   def __len__(self):
-    return max(len(self.data) - self.block_size - self.seqlen, 0)
+    return max(len(self.data) - self.context_size - self.seqlen, 0)
 
   def __getitem__(self, i):
-    offset = i + self.block_size
+    offset = i + self.context_size
     x, y = self.data[i: offset], self.data[offset: offset + self.seqlen]
 
     if self.pad is not None:
