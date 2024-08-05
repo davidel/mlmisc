2ab99780273d3ccd90e95e711f128e907b464c61
Davide Libenzi <davide.libenzi@gmail.com>
2024-08-05 14:31:46 +0200

    Added weight rename utility.

---
+++ mlmisc/bin/weight_rename.py
@@ -0,0 +1,41 @@
+import argparse
+import re
+
+import py_misc_utils.alog as alog
+import torch
+
+
+def main(args):
+  alog.debug(f'Loading {args.input} checkpoint')
+  data = torch.load(args.input)
+
+  repl = []
+  for k in data.keys():
+    for r in args.replace:
+      mx, rx = r.split(',')
+
+      nk = re.sub(mx, rx, k)
+      if nk != k:
+        repl.append((k, nk))
+
+  for k, nk in repl:
+    alog.info(f'Renaming "{k}" to "{nk}"')
+    data[nk] = data[k]
+    data.pop(k)
+
+  if repl:
+    wpath = args.input + '.replace' if args.output is None else args.output
+    alog.debug(f'Saving updated checkpoint to {wpath}')
+    torch.save(data, wpath)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser('Change parameter names from Pytorch checkpoints')
+  parser.add_argument('--input', required=True)
+  parser.add_argument('--replace', nargs='+')
+  parser.add_argument('--output')
+
+  args = parser.parse_args()
+
+  main(args)
+
