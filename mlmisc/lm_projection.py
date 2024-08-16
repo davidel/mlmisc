b9aba4557a376b3c881a9becedb8c1d8814e237d
Davide Libenzi <davide.libenzi@gmail.com>
2024-08-16 10:31:05 +0200

    Bugfix.

--- mlmisc/lm_projection.py
+++ mlmisc/lm_projection.py
@@ -8,7 +8,7 @@ from . import layer_utils as lu
 
 class Projection(nn.Module):
 
-  def __init__(self, context_size, embed_size, embed_heads, vocab_size,
+  def __init__(self, context_size, embed_size, num_heads, vocab_size,
                fc_bias=True,
                act1=None,
                act2=None):
@@ -16,27 +16,27 @@ class Projection(nn.Module):
                  f'Number of heads ({num_heads}) should divide the ' \
                  f'embedding size ({embed_size})')
 
-    embed_k = embed_size // embed_heads
-    prj_q = int(vocab_size // embed_heads)
+    embed_k = embed_size // num_heads
+    prj_q = int(vocab_size // num_heads)
     act1 = act1 or nn.Identity
     act2 = act2 or nn.Identity
 
     super().__init__()
-    self.embed_heads = embed_heads
+    self.num_heads = num_heads
     self.prj1 = nn.Linear(context_size * embed_k, prj_q, bias=fc_bias)
     self.act1 = lu.create(act1)
-    self.prj2 = nn.Linear(embed_heads * prj_q, vocab_size, bias=fc_bias)
+    self.prj2 = nn.Linear(num_heads * prj_q, vocab_size, bias=fc_bias)
     self.act2 = lu.create(act2)
 
   def forward(self, x):
     b, t, c = x.shape
 
     # (B, T, C) => (B, T, CH, CK)
-    y = x.view(b, t, self.embed_heads, -1)
+    y = x.view(b, t, self.num_heads, -1)
     # (B, T, CH, CK) => (B, CH, T, CK)
     y = torch.permute(y, (0, 2, 1, 3))
     # (B, CH, T, CK) => (B, CH, T * CK)
-    y = y.reshape(b, self.embed_heads, -1)
+    y = y.reshape(b, self.num_heads, -1)
     # (B, CH, T * CK) @ (T * CK, Q) => (B, CH, Q)
     y = self.act1(self.prj1(y))
     # (B, CH, Q) => (B, CH * Q)
