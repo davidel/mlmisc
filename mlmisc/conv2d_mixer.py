19c287e9f6aa4c69d6b9f468b08725b8fd72d668
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-06 07:06:21 +0200

    Minor refactoring.

--- mlmisc/conv2d_mixer.py
+++ mlmisc/conv2d_mixer.py
@@ -9,16 +9,15 @@ class Conv2dMixer(nn.Module):
 
   def __init__(self, in_channels, out_channels, convs_spec, act=None):
     act = act or nn.Identity
-    convs = []
+    convs, total_channels = [], 0
     for chans, ksize in convs_spec:
       convs.append(nn.Conv2d(in_channels, chans, kernel_size=ksize, padding='same'))
+      total_channels += ksize
 
     super().__init__()
     self.act = lu.create(act)
     self.convs = ap.ArgsParallel(convs, cat_dim=1)
-    self.conv_proj = nn.Conv2d(sum(s[0] for s in convs_spec), out_channels,
-                               kernel_size=1,
-                               padding='same')
+    self.conv_proj = nn.Conv2d(total_channels, out_channels, kernel_size=1)
 
   def forward(self, x):
     y = self.convs(x)
