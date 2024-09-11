b992b57b476877d99c255bf7673fa2a63a7d58e8
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-11 07:45:58 +0200

    Refactor for code move to py-misc.

--- mlmisc/web_module.py
+++ mlmisc/web_module.py
@@ -1,73 +1,14 @@
 import importlib
 import os
-import shutil
-import subprocess
 import sys
-import tempfile
 import urllib
 
 import py_misc_utils.alog as alog
 import py_misc_utils.assert_checks as tas
+import py_misc_utils.git_repo as pygr
 import py_misc_utils.utils as pyu
 import torch
 import torch.nn as nn
-
-
-class GitRepo:
-
-  def __init__(self, path):
-    self.path = path
-
-  def _git(self, *cmd):
-    git_cmd = ['git', '-C', self.path] + list(cmd)
-    alog.debug(f'Running GIT: {git_cmd}')
-
-    return git_cmd
-
-  def _run(self, *cmd):
-    subprocess.run(cmd, capture_output=True, check=True)
-
-  def _cmd(self, *cmd):
-    self._run(*self._git(*cmd))
-
-  def _outcmd(self, *cmd):
-    return subprocess.check_output(self._git(*cmd))
-
-  def repo(self):
-    return self._outcmd('config', '--get', 'remote.origin.url')
-
-  def clone(self, repo, force=False, shallow=False):
-    do_clone = True
-    if os.path.isdir(self.path):
-      tas.check_eq(repo, self.repo(), f'Repo mismatch!')
-      if force or shallow != self.is_shallow():
-        alog.info(f'Purging old GIT folder: {self.path}')
-        shutil.rmtree(self.path)
-      else:
-        self.pull()
-        do_clone = False
-
-    if do_clone:
-      parent_path = os.path.dirname(self.path)
-      os.makedirs(parent_path, exist_ok=True)
-      if shallow:
-        self._run('git', '-C', parent_path, 'clone', '-q', '--depth', '1',
-                  repo, os.path.basename(self.path))
-      else:
-        self._run('git', '-C', parent_path, 'clone', '-q', repo, os.path.basename(self.path))
-
-  def current_commit(self):
-    return self._outcmd('rev-parse', 'HEAD')
-
-  def is_shallow(self):
-    return self._outcmd('rev-parse', '--is-shallow-repository') == 'true'
-
-  def pull(self):
-    self._cmd('pull', '-q')
-
-  def checkout(self, commit):
-    self._cmd('checkout', '-q', commit)
-
 
 
 def _clone_repo(repo, root, force_clone, commit):
@@ -76,7 +17,7 @@ def _clone_repo(repo, root, force_clone, commit):
 
   rpath = os.path.join(root, upath)
 
-  git = GitRepo(rpath)
+  git = pygr.GitRepo(rpath)
   git.clone(repo, force=force_clone, shallow=commit is None)
   if commit is not None:
     git.checkout(commit)
