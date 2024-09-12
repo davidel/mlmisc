cde003fb5b52a068aec858c5740731ce9936e97e
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-12 20:55:03 +0200

    Added post-norm option.

--- mlmisc/encoder_block.py
+++ mlmisc/encoder_block.py
@@ -12,13 +12,15 @@ class EncoderBlock(nn.Module):
                dim_feedforward=None,
                attn_dropout=None,
                dropout=None,
-               act=None):
+               act=None,
+               pre_norm=True):
     dim_feedforward = dim_feedforward or (4 * input_dim)
     attn_dropout = attn_dropout or 0.0
     dropout = dropout or 0.0
     act = act or nn.GELU
 
     super().__init__()
+    self.pre_norm = pre_norm
     self.attn = atn.Attention(input_dim, num_heads,
                               dropout=attn_dropout)
 
@@ -33,9 +35,13 @@ class EncoderBlock(nn.Module):
     self.norm2 = nn.LayerNorm(input_dim)
 
   def forward(self, x, mask=None):
-    xn = self.norm1(x)
-    x = x + self.attn(xn, xn, xn, mask=mask)
-    x = x + self.linear_net(self.norm2(x))
+    if self.pre_norm:
+      xx = self.norm1(x)
+      x = x + self.attn(xx, xx, xx, mask=mask)
+      x = x + self.linear_net(self.norm2(x))
+    else:
+      x = self.norm1(x + self.attn(x, x, x, mask=mask))
+      x = self.norm2(x + self.linear_net(x))
 
     return x
 
