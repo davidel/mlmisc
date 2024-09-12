522d7d60e997701e96f330b965663cb7962242f6
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-12 21:40:24 +0200

    Minor refactoring.

--- mlmisc/encoder_block.py
+++ mlmisc/encoder_block.py
@@ -1,9 +1,15 @@
+import py_misc_utils.assert_checks as tas
 import torch
 import torch.nn as nn
 
 from . import args_sequential as aseq
 from . import attention as atn
 from . import layer_utils as lu
+
+
+PRE_NORM = 'pre'
+POST_NORM = 'post'
+NORM_MODES = {PRE_NORM, POST_NORM}
 
 
 class EncoderBlock(nn.Module):
@@ -13,14 +19,18 @@ class EncoderBlock(nn.Module):
                attn_dropout=None,
                dropout=None,
                act=None,
-               pre_norm=True):
+               norm_mode=None):
     dim_feedforward = dim_feedforward or (4 * input_dim)
     attn_dropout = attn_dropout or 0.0
     dropout = dropout or 0.0
     act = act or nn.GELU
+    norm_mode = norm_mode or PRE_NORM
+
+    tas.check(norm_mode in NORM_MODES,
+              msg=f'Unknown norm mode "{norm_mode}" (should be one of {NORM_MODES})')
 
     super().__init__()
-    self.pre_norm = pre_norm
+    self.norm_mode = norm_mode
     self.attn = atn.Attention(input_dim, num_heads,
                               dropout=attn_dropout)
 
@@ -35,7 +45,7 @@ class EncoderBlock(nn.Module):
     self.norm2 = nn.LayerNorm(input_dim)
 
   def forward(self, x, mask=None):
-    if self.pre_norm:
+    if self.norm_mode == PRE_NORM:
       xx = self.norm1(x)
       x = x + self.attn(xx, xx, xx, mask=mask)
       x = x + self.linear_net(self.norm2(x))
