c8a12c292c4bef3c727dfa234e6a809e4868a3db
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-14 16:18:35 +0200

    Undo.

--- mlmisc/ensemble.py
+++ mlmisc/ensemble.py
@@ -5,26 +5,25 @@ from . import args_base as ab
 from . import utils as ut
 
 
-class Ensemble(nn.Module):
+class Ensemble(ab.ArgsBase):
 
   def __init__(self, *args, top_n_=None, **kwargs):
-    super().__init__()
+    super().__init__(*args, **kwargs)
     self.top_n = top_n_
-    self.nets = ab.ArgsBase(*args, **kwargs)
-    self.weight = nn.Parameter(torch.full((len(self.nets),), 1 / len(self.nets)))
+    self.weight = nn.Parameter(torch.full((len(self),), 1 / len(self)))
 
   def forward(self, *args, **kwargs):
     if self.training and self.top_n is not None:
-      rnd = torch.randn(len(self.nets))
+      rnd = torch.randn(len(self))
       weights, indices = torch.topk(rnd, self.top_n)
 
-      y, nets = 0, list(self.nets.values())
+      y, nets = 0, list(self.values())
       for i, w in zip(indices, weights):
         y = nets[i](*args, **kwargs) * w * self.weight[i] + y
 
       y = y / torch.sum(weights)
     else:
-      parts = [net(*args, **kwargs) * self.weight[i] for i, net in enumerate(self.nets.values())]
+      parts = [net(*args, **kwargs) * self.weight[i] for i, net in enumerate(self.values())]
       y = ut.add(*parts) / len(parts)
 
     return y
