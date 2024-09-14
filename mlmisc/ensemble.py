52cff98871e6b372740aaa381330c5d716ca2a2d
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-14 15:52:26 +0200

    Using weight for ensemble.

--- mlmisc/ensemble.py
+++ mlmisc/ensemble.py
@@ -10,6 +10,7 @@ class Ensemble(ab.ArgsBase):
   def __init__(self, *args, top_n_=None, **kwargs):
     super().__init__(*args, **kwargs)
     self.top_n = top_n_
+    self.weight = nn.Parameter(torch.full(len(self), 1 / len(self)))
 
   def forward(self, *args, **kwargs):
     if self.training and self.top_n is not None:
@@ -18,11 +19,11 @@ class Ensemble(ab.ArgsBase):
 
       y, nets = 0, list(self.values())
       for i, w in zip(indices, weights):
-        y = nets[i](*args, **kwargs) * w + y
+        y = nets[i](*args, **kwargs) * w * self.weight[i] + y
 
       y = y / torch.sum(weights)
     else:
-      parts = [net(*args, **kwargs) for net in self.values()]
+      parts = [net(*args, **kwargs) * self.weight[i] for i, net in enumerate(self.values())]
       y = ut.add(*parts) / len(parts)
 
     return y
