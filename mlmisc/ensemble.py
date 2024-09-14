a0e54fec5f272e74a1e9fd031166eb24e0ec4f4c
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-14 16:30:10 +0200

    Undo weighting.

--- mlmisc/ensemble.py
+++ mlmisc/ensemble.py
@@ -10,7 +10,6 @@ class Ensemble(ab.ArgsBase):
   def __init__(self, *args, top_n_=None, **kwargs):
     super().__init__(*args, **kwargs)
     self.top_n = top_n_
-    self.weight = nn.Parameter(torch.full((len(self),), 1 / len(self)))
 
   def forward(self, *args, **kwargs):
     if self.training and self.top_n is not None:
@@ -19,11 +18,11 @@ class Ensemble(ab.ArgsBase):
 
       y, nets = 0, list(self.values())
       for i, w in zip(indices, weights):
-        y = nets[i](*args, **kwargs) * w * self.weight[i] + y
+        y = nets[i](*args, **kwargs) * w + y
 
       y = y / torch.sum(weights)
     else:
-      parts = [net(*args, **kwargs) * self.weight[i] for i, net in enumerate(self.values())]
+      parts = [net(*args, **kwargs) for net in self.values()]
       y = ut.add(*parts) / len(parts)
 
     return y
