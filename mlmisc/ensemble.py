0775828e39103d0f456d2f771444fa9f88f6a36b
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-17 08:33:26 +0200

    Added ensemble back.

---
+++ mlmisc/ensemble.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+from . import args_parallel as ap
+from . import utils as ut
+
+
+class Ensemble(nn.Module):
+
+  def __init__(self, nets, loss_fn=None, categorical=None):
+    super().__init__()
+    self.nets = ap.ArgsParallel(nets)
+    self.loss_fn = loss_fn
+    self.categorical = categorical
+
+  def forward(self, *args, targets=None, **kwargs):
+    parts = self.nets(*args, **kwargs)
+
+    if self.categorical == 'voting':
+      y = torch.zeros_like(parts[0])
+      for p in parts:
+        tops = torch.argmax(p, dim=-1)
+        y += torch.nn.functional.one_hot(tops, num_classes=p.shape[-1]).to(y.dtype)
+    else:
+      y = ut.add(parts) / len(parts)
+
+    if self.loss_fn is None:
+      return y
+
+    loss = None
+    if targets is not None:
+      if self.training:
+        loss = ut.add(self.loss_fn(p, targets) for p in parts) / len(parts)
+      else:
+        loss = self.loss_fn(y, targets)
+
+    return y, loss
+
