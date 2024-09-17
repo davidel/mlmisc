08314befbfb00d10f63514ecf30cac05c0775800
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-17 07:52:33 +0200

    Minor refactoring.

--- mlmisc/args_base.py
+++ mlmisc/args_base.py
@@ -1,5 +1,6 @@
 import collections
 
+import py_misc_utils.assert_checks as tas
 import torch
 import torch.nn as nn
 
@@ -15,9 +16,18 @@ def expand_modules(args, kwargs):
     elif isinstance(arg, (list, tuple)):
       net_args = arg
 
-  for i, net in enumerate(net_args):
-    modules[f'{i}'] = net
+  for net in net_args:
+    if isinstance(net, (list, tuple)):
+      tas.check_eq(len(net), 2,
+                   msg=f'In case of list/tuple argument, it must contain two elements')
+      name, enet = net
+
+      tas.check(name not in modules, msg=f'Module "{name}" already present')
+      modules[name] = enet
+    else:
+      modules[f'{len(modules)}'] = net
   for name, net in kwargs.items():
+    tas.check(name not in modules, msg=f'Module "{name}" already present')
     modules[name] = net
 
   return modules
