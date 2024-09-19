2ec30709a28c8a7eb3fb000272e85e03a78e9720
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-19 11:58:22 +0200

    Allow extra kwargs.

--- mlmisc/config.py
+++ mlmisc/config.py
@@ -9,24 +9,26 @@ import torch.nn as nn
 import torch.optim as optim
 
 
-def create_optimizer(params, config):
+def create_optimizer(params, config, **kwargs):
   optim_name, optim_config = pyu.resplit(config, ':')
   optim_config = pyu.parse_dict(optim_config)
+  kwargs.update(optim_config)
 
-  alog.debug(f'Creating {optim_name} optimizer with: {optim_config}')
+  alog.debug(f'Creating {optim_name} optimizer with: {kwargs}')
 
   optim_class = operator.attrgetter(optim_name)(sys.modules[__name__])
 
-  return optim_class(params, **optim_config)
+  return optim_class(params, **kwargs)
 
 
-def create_lr_scheduler(optimizer, config):
+def create_lr_scheduler(optimizer, config, **kwargs):
   sched_name, sched_config = pyu.resplit(config, ':')
   sched_config = pyu.parse_dict(sched_config)
+  kwargs.update(sched_config)
 
-  alog.debug(f'Creating {sched_name} LR scheduler with: {sched_config}')
+  alog.debug(f'Creating {sched_name} LR scheduler with: {kwargs}')
 
   sched_class = operator.attrgetter(sched_name)(sys.modules[__name__])
 
-  return sched_class(optimizer, **sched_config)
+  return sched_class(optimizer, **kwargs)
 
