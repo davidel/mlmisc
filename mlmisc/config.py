897c13ef73a3e9ccc779cd0e68c6238c30956362
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-19 12:45:16 +0200

    Allow non-keyword arguments.

--- mlmisc/config.py
+++ mlmisc/config.py
@@ -13,29 +13,29 @@ from . import reduce_on_plateau as rop
 
 def _config_split(config):
   parts = pyu.resplit(config, ':')
-  mod_config = pyu.parse_dict(parts[1]) if len(parts) == 2 else dict()
+  mod_config = pyu.parse_dict(parts[1], allow_args=True) if len(parts) == 2 else (dict(), [])
 
   return parts[0], mod_config
 
 
 def create_optimizer(params, config, **kwargs):
-  optim_name, optim_config = _config_split(config)
+  optim_name, (optim_config, optim_args) = _config_split(config)
   kwargs.update(optim_config)
 
   alog.debug(f'Creating {optim_name} optimizer with: {kwargs}')
 
   optim_class = operator.attrgetter(optim_name)(sys.modules[__name__])
 
-  return optim_class(params, **kwargs)
+  return optim_class(params, *optim_args, **kwargs)
 
 
 def create_lr_scheduler(optimizer, config, **kwargs):
-  sched_name, sched_config = _config_split(config)
+  sched_name, (sched_config, sched_args) = _config_split(config)
   kwargs.update(sched_config)
 
   alog.debug(f'Creating {sched_name} LR scheduler with: {kwargs}')
 
   sched_class = operator.attrgetter(sched_name)(sys.modules[__name__])
 
-  return sched_class(optimizer, **kwargs)
+  return sched_class(optimizer, *sched_args, **kwargs)
 
