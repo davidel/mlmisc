4eaa5036cab5cf76a7b20603f6d8387207ad19c9
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-23 07:42:27 +0200

    Add local state handling to NetBase.

--- mlmisc/net_base.py
+++ mlmisc/net_base.py
@@ -1,12 +1,16 @@
+import py_misc_utils.assert_checks as tas
+import py_misc_utils.utils as pyu
 import torch
 import torch.nn as nn
 
+
+STATE_KEY = '_NET_STATE'
 
 class NetBase(nn.Module):
 
   def __init__(self, device=None):
     super().__init__()
-    self.device = device or torch.device('cpu')
+    self.device = torch.device(device or 'cpu')
 
   def to(self, *args, **kwargs):
     if args and isinstance(args[0], (str, torch.device)):
@@ -16,9 +20,39 @@ class NetBase(nn.Module):
     else:
       device = self.device
 
-    result =  super().to(*args, **kwargs)
+    result = super().to(*args, **kwargs)
 
     self.device = device
 
     return result
+
+  def state_dict(self, *args, **kwargs):
+    state = super().state_dict(*args, **kwargs)
+
+    net_state_dict = getattr(self, 'net_state_dict', None)
+    if net_state_dict is not None:
+      net_state = state.get(STATE_KEY)
+      if net_state is None:
+        state[STATE_KEY] = net_state = dict()
+
+      for name, value in net_state_dict().items():
+        tas.check(name not in net_state, msg=f'State "{name}" already exists')
+        net_state[name] = value
+
+    return state
+
+  def load_state_dict(self, state, *args, **kwargs):
+    net_load_state_dict = getattr(self, 'net_load_state_dict', None)
+    if net_load_state_dict is not None:
+      net_state = state.get(STATE_KEY)
+
+      net_load_state_dict(net_state)
+
+      if not net_state:
+        state.pop(STATE_KEY)
+
+    return super().load_state_dict(state, *args, **kwargs)
+
+  def pop_net_state(self, state, names):
+    return (state.pop(name, None) for name in pyu.as_sequence(names))
 
