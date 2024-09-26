75c42342bd2b4f2a3873957a7efcedcdef850eef
Davide Libenzi <davide.libenzi@gmail.com>
2024-09-26 18:02:19 +0200

    Added test sequence models.

---
+++ mlmisc/models/sequence/sequence_base.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+from ... import loss_wrappers as lsw
+
+
+class SequenceBase(nn.Module):
+
+  def __init__(self, context_size, embed_size):
+    super().__init__()
+    self.tok_emb = nn.Embedding(config.vocab_size, embed_size)
+    self.pos_emb = nn.Parameter(torch.zeros((1, context_size, embed_size)))
+    self.loss = lsw.SeqLoss(nn.CrossEntropyLoss())
+
+  def forward(self, x):
+    y = self.tok_emb(x) + self.pos_emb
+
+    return y
+
---
+++ mlmisc/models/sequence/shard_seq.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+
+from ... import args_sequential as aseq
+from ... import layer_utils as lu
+from ... import sequence_utils as sequ
+from ... import shard_attention as satn
+
+from . import sequence_base as sb
+
+
+class ShardSeq(sb.SequenceBase):
+
+  def __init__(self, context_size, embed_size, num_heads, vocab_size, num_layers,
+               use_attn_mask=None,
+               act=None):
+    use_attn_mask = True if use_attn_mask is None else use_attn_mask
+    act = act or 'gelu'
+
+    def post():
+      return aseq.ArgsSequential(
+        nn.LayerNorm(embed_size),
+        lu.create(act),
+      )
+
+    super().__init__(context_size, embed_size)
+    self.blocks = aseq.ArgsSequential(
+      [satn.ShardAttention(embed_size, num_heads,
+                           post=post,
+                           post_feed=lambda x, y: x + y)
+       for _ in range(num_layers)]
+    )
+    self.vocab_head = sequ.build_vocab_head(embed_size, vocab_size,
+                                            activation=act)
+    if use_attn_mask:
+      self.mask = torch.triu(torch.ones(context_size, context_size),
+                             diagonal=1).bool()
+    else:
+      self.mask = None
+
+  def forward(self, x, targets=None):
+    y = super().forward(x)
+
+    mask = self.mask.to(y.device) if self.mask is not None else None
+
+    y = self.blocks(y, mask=mask)
+    # (B, T, C) @ (C, V) => (B, T, V)
+    y = self.vocab_head(y)
+
+    return y, self.loss(y, targets)
+
