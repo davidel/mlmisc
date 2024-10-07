f79e9c4221f309a130e51b4f6b9c6146638a26c1
Davide Libenzi <davide.libenzi@gmail.com>
2024-10-07 18:51:06 +0200

    Undo.

--- mlmisc/nn_lambda.py
+++ mlmisc/nn_lambda.py
@@ -1,20 +1,17 @@
-import inspect
-
 import torch
 import torch.nn as nn
 
 
 class Lambda(nn.Module):
 
-  def __init__(self, fn):
+  def __init__(self, fn, info=None):
     super().__init__()
     self.fn = fn
+    self.info = info
 
   def forward(self, *args, **kwargs):
     return self.fn(*args, **kwargs)
 
   def extra_repr(self):
-    source = inspect.getsource(self.fn)
-
-    return f'source={source}'
+    return self.info or ''
 
