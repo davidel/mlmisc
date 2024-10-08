0e2919cc7179eaf72f08d9554106cf9a75f084f1
Davide Libenzi <davide.libenzi@gmail.com>
2024-10-08 19:29:31 +0200

    Refactoring.

--- mlmisc/model_wrappers.py
+++ mlmisc/model_wrappers.py
@@ -14,6 +14,7 @@ import py_misc_utils.num_utils as pynu
 import py_misc_utils.utils as pyu
 
 from . import config as conf
+from . import layer_utils as lu
 from . import loss_wrappers as lsw
 from . import module_builder as mbld
 from . import utils as ut
@@ -55,38 +56,39 @@ class Web(nn.Module):
     return y, self.loss(y, targets)
 
 
-class HugginFaceImgTune(nn.Module):
+class HugginFaceModel(nn.Module):
 
-  def __init__(self, model_name, model_class, loss, num_classes,
-               output_collate=None,
+  def __init__(self, model_name, model_class, processor_class,
                cache_dir=None):
-    mclass = getattr(trs, model_class)
-    model = mclass.from_pretrained(
+    model_class = getattr(trs, model_class)
+    model = model_class.from_pretrained(
       model_name,
       trust_remote_code=False,
       cache_dir=cache_dir)
 
-    image_processor = trs.AutoImageProcessor.from_pretrained(
+    processor_class = getattr(trs, processor_class)
+    processor = processor.from_pretrained(
       model_name,
       use_fast=True,
       trust_remote_code=False,
       cache_dir=cache_dir)
 
-    alog.info(f'HF Model:\n{model}')
-    alog.info(f'Model Config:\n{model.config}')
-    alog.info(f'Image Processor:\n{image_processor}')
-
-    hidden_size = max(64 * num_classes, 4 * model.config.hidden_size)
+    alog.debug(f'HF Model:\n{model}')
+    alog.debug(f'Model Config:\n{model.config}')
 
     super().__init__()
-    self.loss = lsw.CatLoss(conf.create_loss(loss))
-    self.output_collate = output_collate
-    self.ctx = pyu.make_object(model=model, image_processor=image_processor)
-    self.head = mbld.ModuleBuilder((self.ctx.model.config.hidden_size,))
-    self.head.linear(hidden_size)
-    self.head.add(nn.Dropout(0.2))
-    self.head.add(nn.ReLU(inplace=True))
-    self.head.linear(num_classes)
+    # Storing model inside an object prevents nn.Module machinery to save its
+    # weights when checkpointing, which is what we want as they are frozen.
+    self.ctx = pyu.make_object(model=model, processor=processor)
+
+  def model(self):
+    return self.ctx.model
+
+  def config(self):
+    return self.ctx.model.config
+
+  def processor(self):
+    return self.ctx.processor
 
   def to(self, *args, **kwargs):
     res = super().to(*args, **kwargs)
@@ -94,19 +96,50 @@ class HugginFaceImgTune(nn.Module):
 
     return res
 
+  def forward(self, *args, **kwargs):
+    with torch.no_grad():
+      if args:
+        model_input = self.ctx.processor(*args)
+      else:
+        model_input = kwargs
+
+      y = self.ctx.model(**model_input, output_hidden_states=True)
+
+      return y.last_hidden_state
+
+
+class HugginFaceImgTune(HugginFaceModel):
+
+  def __init__(self, model_name, model_class, loss, num_classes,
+               processor_class=None,
+               output_collate=None,
+               dropout=None,
+               act=None,
+               cache_dir=None):
+    processor_class = pyu.value_or(processor_class, 'AutoImageProcessor')
+    output_collate = pyu.value_or(output_collate, nn.Identity())
+    dropout = pyu.value_or(dropout, 0.1)
+    act = pyu.value_or(act, nn.ReLU)
+
+    super().__init__(model_name, model_class, processor_class,
+                     cache_dir=cache_dir)
+
+    alog.debug(f'Image Processor:\n{self.processor()}')
+
+    hidden_size = max(64 * num_classes, 4 * self.config().hidden_size)
+
+    self.loss = lsw.CatLoss(conf.create_loss(loss))
+    self.output_collate = output_collate
+    self.head = mbld.ModuleBuilder((self.config().hidden_size,))
+    self.head.linear(hidden_size)
+    self.head.add(nn.Dropout(dropout))
+    self.head.add(lu.create(act))
+    self.head.linear(num_classes)
+
   def forward(self, x, targets=None):
     with torch.no_grad():
-      px = self.ctx.image_processor(x)
-      vouts = self.ctx.model(**px, output_hidden_states=True)
-
-      if self.output_collate == 'first':
-        y = vouts.last_hidden_state[:, 0, :]
-      elif self.output_collate == 'last':
-        y = vouts.last_hidden_state[:, -1, :]
-      elif self.output_collate == 'mean':
-        y = einops.reduce(vouts.last_hidden_state, 'b n h -> b h', 'mean')
-      else:
-        y = vouts.last_hidden_state[:, int(self.output_collate), :]
+      y = super().forward(x)
+      y = self.output_collate(y)
 
     y = self.head(y)
 
