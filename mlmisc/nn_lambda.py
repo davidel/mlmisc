0a650a1cb329ae8580fe944bbf259a283a740c0a
Davide Libenzi <davide.libenzi@gmail.com>
2024-10-11 07:15:35 +0200

    Allow Lmabda to be passed context.

--- mlmisc/nn_lambda.py
+++ mlmisc/nn_lambda.py
@@ -1,13 +1,14 @@
+import py_misc_utils.utils as pyu
 import torch
 import torch.nn as nn
 
 
 class Lambda(nn.Module):
 
-  def __init__(self, fn, info=None):
+  def __init__(self, fn, info=None, env=None):
     super().__init__()
     if isinstance(fn, str):
-      self.fn = eval(f'lambda {fn}')
+      self.fn = eval(f'lambda {fn}', env)
       self.info = info or f'lambda {fn}'
     else:
       self.fn = fn
