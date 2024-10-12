7267c50d64abb4690686dc20f024fee8e3a5e376
Davide Libenzi <davide.libenzi@gmail.com>
2024-10-12 15:56:48 +0200

    Added cross-linear layer.

---
+++ mlmisc/cross_linear.py
@@ -0,0 +1,31 @@
+import einops
+import mlmisc.layer_utils as lu
+import py_misc_utils.utils as pyu
+import torch
+import torch.nn as nn
+
+
+class CrossLinear(nn.Module):
+
+  def __init__(self, context_size, embed_size, act=None):
+    act = pyu.value_or(act, nn.Identity)
+
+    super().__init__()
+    self.fc = nn.Linear(embed_size, embed_size)
+    self.alt_fc = nn.Linear(context_size, context_size)
+    self.act = lu.create(act)
+
+  def forward(self, x):
+    y = self.fc(x)
+    xa = einops.rearrange(x, 'b c e -> b e c')
+    ya = self.alt_fc(xa)
+    ya = einops.rearrange(ya, 'b e c -> b c e')
+    y += ya
+    y = self.act(y)
+
+    return y
+
+  def extra_repr(self):
+    return pyu.stri(dict(context_size=self.alt_fc.weight.shape[-1],
+                         embed_size=self.fc.weight.shape[-1]))
+
