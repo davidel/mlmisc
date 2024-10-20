de597d85523d1d10c664154f7fefde5c681b3021
Davide Libenzi <davide.libenzi@gmail.com>
2024-10-20 15:08:31 +0200

    Added ability to pass embedding init data.

--- mlmisc/models/sequence/sequence_base.py
+++ mlmisc/models/sequence/sequence_base.py
@@ -1,3 +1,5 @@
+import py_misc_utils.assert_checks as tas
+import py_misc_utils.utils as pyu
 import torch
 import torch.nn as nn
 
@@ -8,10 +10,22 @@ from ... import net_base as nb
 class SequenceBase(nb.NetBase):
 
   def __init__(self, context_size, embed_size, vocab_size,
+               embeddings=None,
+               freeze=None,
                padding_idx=None):
+    freeze = pyu.value_or(freeze, False)
+
     super().__init__()
-    self.tok_emb = nn.Embedding(vocab_size, embed_size,
-                                padding_idx=padding_idx)
+    if embeddings is None:
+      self.tok_emb = nn.Embedding(vocab_size, embed_size,
+                                  padding_idx=padding_idx)
+    else:
+      tas.check_eq(tuple(embeddings.shape), (vocab_size, embed_size),
+                   msg=f'Wrong embeddings shape: {tuple(embeddings.shape)} vs. ' \
+                   f'{(vocab_size, embed_size)}')
+      self.tok_emb = nn.Embedding.from_pretrained(embeddings,
+                                                  freeze=freeze,
+                                                  padding_idx=padding_idx)
     self.pos_emb = nn.Parameter(torch.zeros((1, context_size, embed_size)))
     self.loss = lsw.SeqLoss(nn.CrossEntropyLoss())
 
