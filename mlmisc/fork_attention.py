4543b173de9b0e649f90a413e8fc381b6448f85e
Davide Libenzi <davide.libenzi@gmail.com>
2024-11-04 09:24:24 +0100

    Added ForkAttention based Viz model.

--- mlmisc/fork_attention.py
+++ mlmisc/fork_attention.py
@@ -1,29 +1,38 @@
 import math
 
 import einops
+import py_misc_utils.alog as alog
 import py_misc_utils.utils as pyu
 import torch
 import torch.nn as nn
 
-from . import layer_utils as lu
-from .models.sequence import sequence_base as sb
 from . import utils as ut
+
+
+def get_post_feed(post_feed):
+  if isinstance(post_feed, str):
+    if post_feed == 'x+y':
+      return lambda x, y: x + y
+    elif post_feed == 'y':
+      return lambda x, y: y
+    else:
+      alog.xraise(ValueError, f'Invalid post-feed: {post_feed}')
+  elif callable(post_feed):
+    return post_feed
+
+  return post_feed or lambda x, y: y
 
 
 class ForkAttention(nn.Module):
 
   def __init__(self, context_size, embed_size,
-               post=None,
                post_feed=None):
-    post = pyu.value_or(post, nn.Identity)
-
     super().__init__()
     self.fc = nn.Linear(embed_size, embed_size)
     self.alt_fc = nn.Linear(context_size, context_size)
     self.vfc = nn.Linear(embed_size, embed_size)
     self.attend = nn.Softmax(dim=-1)
-    self.post = lu.create(post)
-    self.post_feed = pyu.value_or(post_feed, lambda x, y: y)
+    self.post_feed = get_post_feed(post_feed)
 
   def forward(self, x, mask=None):
     # (B, C, E) -> (B, C, E)
@@ -44,7 +53,7 @@ class ForkAttention(nn.Module):
     # (B, C, C) @ (B, C, E) -> (B, C, E)
     y = torch.einsum('bck,bke->bce', attn, vx)
 
-    return self.post(self.post_feed(x, y))
+    return self.post_feed(x, y)
 
   def extra_repr(self):
     return ut.extra_repr(context_size=self.alt_fc.weight.shape[-1],
---
+++ mlmisc/models/vision/fork_viz_seq.py
@@ -0,0 +1,49 @@
+import einops.layers.torch as einpt
+import py_misc_utils.alog as alog
+import py_misc_utils.assert_checks as tas
+import py_misc_utils.num_utils as pynu
+import py_misc_utils.utils as pyu
+import torch
+
+from ... import conv_utils as cu
+from ... import fork_attention as fa
+from ... import layer_utils as lu
+from ... import module_builder as mb
+from ... import utils as ut
+
+from . import vit_base as vb
+
+
+class ForkVizSeq(vb.ViTBase):
+
+  def __init__(self, shape, embed_size, num_classes, num_layers,
+               convs=None,
+               result_tiles=None,
+               act=None,
+               weight=None,
+               label_smoothing=None):
+    result_tiles = pyu.value_or(result_tiles, 2)
+    act = pyu.value_or(act, 'gelu')
+
+    if isinstance(convs, str):
+      convs = cu.convs_from_string(convs)
+
+    patcher = cu.build_conv_stack(convs, shape=shape)
+    patcher.add(einpt.Rearrange('b c h w -> b (h w) c'))
+    patcher.linear(embed_size)
+    patcher.add(lu.create(act))
+
+    alog.debug(f'Input shape {shape}, patcher shape {patcher.shape}')
+
+    net = mb.ModuleBuilder((patcher.shape[0] + result_tiles, patcher.shape[1]))
+    for i in range(num_layers):
+      net.add(fa.ForkAttention(*net.shape, post_feed='x+y'))
+      net.add(lu.create(act))
+      rid = net.layernorm()
+
+    super().__init__(patcher, net, shape, embed_size, num_classes,
+                     result_tiles=result_tiles,
+                     act=act,
+                     weight=weight,
+                     label_smoothing=label_smoothing)
+
