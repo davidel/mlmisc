5b5954b9d80a3724a14222b667303f4b2682a514
Davide Libenzi <davide.libenzi@gmail.com>
2024-11-13 15:12:22 +0100

    Added image concat API.

--- mlmisc/utils.py
+++ mlmisc/utils.py
@@ -324,6 +324,29 @@ def reset_lr(optimizer, lr):
       pgrp['lr'] = lr
 
 
+def image_pad_concat(*imgs, channels_dim=None):
+  channels_dim = pyu.value_or(channels_dim, -3)
+  if channels_dim > 0 and imgs:
+    channels_dim = -(imgs[0].ndim - channels_dim)
+
+  max_dims = [-1] * abs(channels_dim + 1)
+  for img in imgs:
+    for i, dim in enumerate(range(-1, channels_dim, -1)):
+      max_dims[i] = max(max_dims[i], img.shape[dim])
+
+  padded_imgs = []
+  for img in imgs:
+    pad = []
+    for i, dim in enumerate(range(-1, channels_dim, -1)):
+      diff = max_dims[i] = img.shape[dim]
+      pad.append(diff // 2)
+      pad.append(diff - pad[-1])
+
+    padded_imgs.append(nn.functional.pad(img, pad))
+
+  return torch.cat(padded_imgs, dim=channels_dim)
+
+
 def minmax_bbox(bbox):
   # Boxes are (N, 4) shapes with (min_x, min_y, xsize, ysize)
   mmbox = torch.clone(bbox)
