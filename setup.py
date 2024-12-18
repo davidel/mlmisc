316e8f94acfe4da8daca9ea5c2543cbff141f039
Davide Libenzi <davide.libenzi@gmail.com>
2024-12-18 16:41:20 +0100

    Added dependency.

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ from setuptools import setup, find_packages
 
 
 setup(name='mlmisc',
-      version='0.3',
+      version='0.38',
       description='Miscellaneous ML Utilities',
       author='Davide Libenzi',
       packages=find_packages(),
       install_requires=[
-          'torch',
-          'torchvision',
+          'datasets',
+          'einops',
+          'msgpack',
           'numpy',
           'Pillow',
           'py_misc_utils @ git+https://github.com/davidel/py_misc_utils',
-          'einops',
-          'datasets',
+          'torch',
+          'torchvision',
       ],
       )
