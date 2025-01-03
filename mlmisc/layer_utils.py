eb1689025276b74eaa0a4b894092956178e4a467
Davide Libenzi <davide.libenzi@gmail.com>
2025-01-03 11:36:23 +0100

    Moved modules.

--- mlmisc/layer_utils.py
+++ mlmisc/layer_utils.py
@@ -38,37 +38,3 @@ def create(lay):
   else:
     alog.xraise(ValueError, f'Unknown layer specification: {lay}')
 
-
-class Wrap(nn.Module):
-
-  def __init__(self, net):
-    super().__init__()
-    self.net = net
-    self.res = None
-
-  def forward(self, *args, **kwargs):
-    self.res = self.net(*args, **kwargs)
-
-    return self.res
-
-
-class Mix(nn.Module):
-
-  def __init__(self, func, *nets):
-    super().__init__()
-    self.func = func
-    self.nets = nets
-    self.res = None
-
-  def forward(self, *args):
-    fargs = list(args) + [net.res for net in self.nets]
-
-    self.res = self.func(*fargs)
-
-    return self.res
-
-  def extra_repr(self):
-    reprs = ',\n'.join(repr(net) for net in self.nets)
-
-    return reprs
-
