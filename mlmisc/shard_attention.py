96760bac45a343417c44c7816648bf21782a9f1a
Davide Libenzi <davide.libenzi@gmail.com>
2025-04-29 18:06:04 +0200

    Testing SDPA instead of manual coding.

--- mlmisc/shard_attention.py
+++ mlmisc/shard_attention.py
@@ -22,27 +22,15 @@ class ShardAttention(nn.Module):
     super().__init__()
     self.num_heads = num_heads
     self.weight = cu.kuni_parameter(num_heads * embed_size, embed_size)
-    self.attend = nn.Softmax(dim=-1)
     self.post = lu.create(post)
     self.post_feed = post_feed
 
   def forward(self, x, mask=None):
-    y = einops.rearrange(x, 'b t (nh hs) -> b nh t hs', nh=self.num_heads)
-    yt = einops.rearrange(y, 'b nh t hs -> b nh hs t')
-    # (B, NH, T, HS) @ (B, NH, HS, T) => (B, NH, T, T)
-    y = torch.einsum('bhtk,bhks->bhts', y, yt)
-
-    if mask is not None:
-      y = y.masked_fill(mask, float('-inf'))
-    # The scaled dot product dimension to be used is the head size "hs" (yt.shape[-2]).
-    y = self.attend(y / math.sqrt(yt.shape[-2]))
-
-    xx = einops.repeat(x, 'b t c -> b nh t c', nh=self.num_heads)
-    # (B, NH, T, T) @ (B, NH, T, C) => (B, NH, T, C)
-    y = torch.einsum('bhts,bhsk->bhtk', y, xx)
+    q = k = einops.rearrange(x, 'b t (nh hs) -> b nh t hs', nh=self.num_heads)
+    v = einops.repeat(x, 'b t c -> b nh t c', nh=self.num_heads)
+    y = nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=mask)
     y = einops.rearrange(y, 'b nh t c -> b t (nh c)')
-    # (B, T, NH * C) @ (NH * C, C) => (B, T, C)
-    y = y @ self.weight
+    y = y @ weight
 
     return self.post(self.post_feed(x, y))
 
