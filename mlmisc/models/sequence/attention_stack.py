ad2d716ec12cf88eedd86103f3d706e6829638fb
Davide Libenzi <davide.libenzi@gmail.com>
2025-05-01 13:25:03 +0200

    Create a more generic model generator.

--- mlmisc/models/sequence/attention_stack.py
+++ mlmisc/models/sequence/attention_stack.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 
 from ... import args_sequential as aseq
+from ... import config as conf
 from ... import layer_utils as lu
 from ... import module_builder as mb
 from ... import sequence_utils as sequ
@@ -11,11 +12,13 @@ from ... import shard_attention as satn
 from . import sequence_base as sb
 
 
-def create_net(context_size, embed_size, num_heads, num_layers, shortcut, act):
+def create_net(attn_config, context_size, embed_size, num_heads, num_layers,
+               shortcut, act):
   net = mb.ModuleBuilder((context_size, embed_size))
   result_ids = [net.last_id()]
   for i in range(num_layers):
-    rid = net.add(satn.ShardAttention(embed_size, num_heads),
+    attn = conf.create_object('Attention Layer', attn_config, embed_size, num_heads)
+    rid = net.add(attn,
                   net_args=('mask',),
                   input_fn=mb.inputsum_back(result_ids, back=shortcut))
     result_ids.append(rid)
@@ -25,16 +28,18 @@ def create_net(context_size, embed_size, num_heads, num_layers, shortcut, act):
   return net
 
 
-class ShardSeq(sb.SequenceBase):
+class AttentionStack(sb.SequenceBase):
 
   def __init__(self, context_size, embed_size, num_heads, vocab_size, num_layers,
+               attn_config='mlmisc.attention.create:is_self=True',
                use_attn_mask=True,
                shortcut=1,
                act='gelu',
                vocab_final='linear'):
     super().__init__(context_size, embed_size, vocab_size)
 
-    self.net = create_net(context_size, embed_size, num_heads, num_layers, shortcut, act)
+    self.net = create_net(attn_config, context_size, embed_size, num_heads, num_layers,
+                          shortcut, act)
     self.net.add(sequ.build_vocab_head(embed_size, vocab_size,
                                        act=act,
                                        final=vocab_final))
